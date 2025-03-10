50b8892108af0ffc2c2513807919afd6aa039afb
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-03-09 18:06:02 -0700

    pixel perfect

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -81,7 +81,6 @@ const SmallText = styled.h2`
 const PhotoWrapper = styled.div`
     position: relative;
     z-index: 0;
-    margin-top: 65px;
 
     &::before {
         content: "";
@@ -101,6 +100,10 @@ const PhotoWrapper = styled.div`
             top: -17px;
             left: 20px
         }
+    }
+    
+    @media ${Theme.media.mobile} {
+        margin-top: 65px;
     }
 `
 
