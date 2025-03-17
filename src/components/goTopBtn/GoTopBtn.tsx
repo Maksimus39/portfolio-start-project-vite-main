a18bf7cf54798b2dac7f5aaaaebc92f4f3cf997a
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-03-17 08:46:31 -0700

    add goTopBtn

---
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import {Icon} from "../icon/Icon.tsx";
+
+export const GoTopBtn = () => {
+    return (
+        <StyledGoTopBtn>
+            <Icon iconId={""}/>
+        </StyledGoTopBtn>
+    );
+};
+
+
+const StyledGoTopBtn = styled.button`
+
+`
