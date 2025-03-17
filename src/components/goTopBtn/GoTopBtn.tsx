b61b0828352451cab939c53eb7b800d74c70bb92
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-03-17 09:16:35 -0700

    add goTopBtnUp commit

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -1,11 +1,34 @@
 import styled from "styled-components";
 import {Icon} from "../icon/Icon.tsx";
+import {animateScroll as scroll} from "react-scroll";
+import {useEffect, useState} from "react";
 
 export const GoTopBtn = () => {
+    const [showButton, setShowButton] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 200) {
+                setShowButton(true)
+            } else {
+                setShowButton(false)
+            }
+        });
+    }, [showButton])
+
+    const goToBtnHandler = () => {
+        scroll.scrollToTop()
+    }
+
+
     return (
-        <StyledGoTopBtn>
-            <Icon iconId={"goTopBtn"} height={"15"} width={"16"} viewBox={"0 0 16 15"}/>
-        </StyledGoTopBtn>
+        <>
+            {
+                showButton && <StyledGoTopBtn onClick={goToBtnHandler}>
+                    <Icon iconId={"goToBtnUp"} height={"15"} width={"16"} viewBox={"0 0 16 15"}/>
+                </StyledGoTopBtn>
+            }
+        </>
     );
 };
 
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,10 +2,13 @@ import {Icon} from "../icon/Icon.tsx";
 import {animateScroll as scroll} from "react-scroll"
 
 export const Logo = () => {
+
+    const scrollToTopHandler = () => {
+        scroll.scrollToTop()
+    }
+
     return (
-        <a onClick={() => {
-            scroll.scrollToTop()
-        }}>
+        <a onClick={scrollToTopHandler}>
             <Icon iconId={'react'}/>
         </a>
     );
