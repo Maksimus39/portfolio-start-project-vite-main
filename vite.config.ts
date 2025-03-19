cb5f62ae5b29ece94ad3e47f533e9d5e4777f6e0
Max <maksim.minakov.83@mail.ru>
2025-03-19 12:35:31 +0300

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-start-project-vite-main/', // Укажите имя вашего репозитория
+  base: '/portfolio-start-project-vite-main/', // Убедитесь, что это совпадает с именем репозитория
 });
